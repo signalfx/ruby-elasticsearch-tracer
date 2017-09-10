88011954037bab3e61cc15157997e9a12c568bdd
iaintshine <bodziomista@gmail.com>
2017-09-11 00:25:03 +0200

    Add instrument helper method, same as in other tracers

--- lib/elasticsearch/tracer.rb
+++ lib/elasticsearch/tracer.rb
@@ -1,7 +1,14 @@
+require 'elasticsearch'
+
 require "elasticsearch/tracer/version"
 require "elasticsearch/tracer/transport"
 
 module Elasticsearch
   module Tracer
+    class << self
+      def instrument(tracer: OpenTracing.global_tracer, active_span: nil, transport:)
+        Elasticsearch::Tracer::Transport.new(tracer: tracer, active_span: active_span, transport: transport)
+      end
+    end
   end
 end
