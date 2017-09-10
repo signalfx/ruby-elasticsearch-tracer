81c709e7024a1a47092518b5eb9aff7734fbb6a0
iaintshine <bodziomista@gmail.com>
2017-09-11 00:24:44 +0200

    Add pass through Transport implementation

--- lib/elasticsearch/tracer/transport.rb
+++ lib/elasticsearch/tracer/transport.rb
@@ -1,6 +1,17 @@
 module Elasticsearch
   module Tracer
     class Transport
+      attr_reader :tracer, :active_span, :wrapped
+
+      def initialize(tracer: OpenTracing.global_tracer, active_span: nil, transport:)
+        @tracer = tracer
+        @active_span = active_span
+        @wrapped = transport
+      end
+
+      def perform_request(method, path, params={}, body=nil)
+        @wrapped.perform_request(method, path, params, body)
+      end
     end
   end
 end
