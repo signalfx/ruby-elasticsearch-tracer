1ee80a40946fffe6df72a3ffbd94462c1ea2495c
Ashwin Chandrasekar <achandrasekar@signalfx.com>
2018-12-04 12:04:35 -0500

    Remove tag_body that is no longer needed

--- lib/elasticsearch/tracer.rb
+++ lib/elasticsearch/tracer.rb
@@ -7,8 +7,6 @@ require "elasticsearch/tracer/tracing_client"
 module Elasticsearch
   module Tracer
     class << self
-      attr_accessor :tag_body
-
       def instrument(tracer: OpenTracing.global_tracer, active_span: nil, transport:)
         Elasticsearch::Tracer::Transport.new(tracer: tracer, active_span: active_span, transport: transport)
       end
